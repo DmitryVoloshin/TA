8a1c06fb533db72da67f634099b86971d240ca75
Dmitriy Voloshin <voloshincode@gmail.com>
2021-05-21 18:12:25 +0300

    styles for footer contact block

--- src/components/block/contact/ContactBlock.jsx
+++ src/components/block/contact/ContactBlock.jsx
@@ -1,8 +1,26 @@
 import React from 'react';
 
+import './ContactBlock.less'
+
 const ContactBlock = ( ) =>{
     return (
-        <div>Contact Block</div>
+        <div className="contact-block">
+            <h2 className="contact-block_title">CONTACT US</h2>     
+            <p className="contact-second_title">WANT TO BECOME A PILOT?</p>
+            <p className="contact-main_text">
+                nquietude simplicity terminated she compliment remarkablyfew her nay. 
+                The weeksInquietude simplicity terminated she compliment remarkablyfew her nay. 
+                The weeksInquietude simplicity terminated she compliment remarkablyfe
+            </p>
+            <div className="contact-block_buttons">
+                <button className="contact-block_button white">
+                    CONTACT US
+                </button>
+                <button className="contact-block_button red">
+                    E-REGISTRATION
+                </button>
+            </div>
+         </div>
     )
 }
 
