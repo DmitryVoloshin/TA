9fa0a8dc2518d6cf2d4bc273a25ac47ad9d81a80
Dmitriy Voloshin <voloshincode@gmail.com>
2021-05-21 10:49:38 +0300

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+import './app.less'
+
+
+
+
+const App = ( ) =>{
+
+
+
+    return(
+        <div className="app">
+            <h2>hey</h2>
+        </div>
+    )
+}
+
+export default App
